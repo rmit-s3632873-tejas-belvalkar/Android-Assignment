a53cf1804c7ed118229450d686b65c874448cc6e
rmit-s3632873-tejas-belvalkar <s3632873@student.rmit.edu.au>
2018-07-22 23:36:00 +1000

    zsafdxc vbn

--- MyApplication3/app/src/main/java/com/example/tejas/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/tejas/myapplication/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.tejas.myapplication;
 
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
