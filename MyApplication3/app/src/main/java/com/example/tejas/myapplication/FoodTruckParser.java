ed81747f80edd2e5275e9f42df17bd229b839f22
rmit-s3632873-tejas-belvalkar <s3632873@student.rmit.edu.au>
2018-07-23 17:36:30 +1000

    ...

---
+++ MyApplication3/app/src/main/java/com/example/tejas/myapplication/FoodTruckParser.java
@@ -0,0 +1,14 @@
+package com.example.tejas.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodTruckParser {
+
+    private List<Trackable> trackables;
+    public List<Trackable> getTrackableList;{
+        trackables = new ArrayList<>();
+
+    }
+
+}
--- MyApplication3/app/src/main/java/com/example/tejas/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/tejas/myapplication/MainActivity.java
@@ -55,15 +55,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner spinner = findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
     }
 
     public void buttonOnClick(View view) {
